bb59aa8250b21e379878f32d827e1118d315e486
Tannushree Vaishnav <vaishnavtannushree@gmail.com>
2026-04-03 16:04:33 +0530

    Data Transformation completed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+
 import sys
 if __name__=="__main__":
     try:
@@ -19,7 +21,16 @@ if __name__=="__main__":
         data_validation_artifact=data_validation.initiate_data_vallidation()
         logging.info("Data validation completed and artifact")
         print(data_validation_artifact)
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        logging.info("Initiated data transformation")
         
+        data_transformation = DataTransformation(
+                                data_validation_artifact,
+                                data_transformation_config
+                )
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data transformation completed and artifact")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
 
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -44,4 +44,35 @@ def write_yaml_file(file_path:str,content: object,replace: bool=True)->None:
     except Exception as e:
         raise NetworkSecurityException(e, sys)
     
-        
+def save_numpy_array_data(file_path: str,array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,'wb') as file_obj:
+            np.save(file_obj,array)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
+    
+def save_object(file_path: str,obj: object)->None:
+    """
+    Save a Python object to a file using pickle.
+    Args:
+        file_path (str): The path to the file where the object will be saved.
+        obj (object): The Python object to be saved.
+    Returns: None
+    Raises:
+        NetworkSecurityException: If there is an error saving the object to the file.
+    """
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Exited the save_object method of MainUtils class")    
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
